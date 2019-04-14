20d02cbacbd83c17947b710d4ce8ce50593c1bc2
root <omid.javaheri66@gmail.com>
2019-04-14 12:58:54 +0430

    feedgram list

--- www/js/feedgramListCtrl.js
+++ www/js/feedgramListCtrl.js
@@ -7,6 +7,32 @@ try {
 mymodule.controller('feedgramListCtrl', function ($scope, $compile, $http, localStorageService, $parse, $rootScope, $state) {
   $rootScope.pageTitle = 'فیدگرام';
   $rootScope.currentMobileActiveMenu = "feedgram";
+  $scope.page = 0;
+  $scope.size = 10;
+
+  setTimeout(function () {
+    startLoading();
+    var token = localStorageService.get("my_access_token");
+    var httpOptions = {
+      headers: { 'Content-type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + token }
+    };
+    var params = {
+      "id": "57",
+      "pageableDTO": {
+        "direction": 'ASC',
+        "page": $scope.page,
+        "size": $scope.size,
+        "sortBy": 'id'
+      }
+    };
+    $http.post("http://127.0.0.1:9000/v1/feedgram/employee/getPostList", params, httpOptions)
+      .success(function (data, status, headers, config) {
+        $scope.posts = data.data;
+        stopLoading();
+      }).catch(function (err) {
+      $rootScope.handleError(params, "/feedgram/employee/post", err, httpOptions);
+    });
+  },700);
 
   $scope.goToDetail = function () {
     $state.go("feedgram-detail");
