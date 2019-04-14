1d7d31cfd682293e18db6d029199c16fbd6220e6
root <omid.javaheri66@gmail.com>
2019-04-14 16:52:23 +0430

    feedgram profile

--- www/js/feedgramProfileCtrl.js
+++ www/js/feedgramProfileCtrl.js
@@ -9,5 +9,27 @@ mymodule.controller('feedgramProfileCtrl', function ($scope, $compile, $http, lo
   $rootScope.pageTitle = 'پروفایل';
   $rootScope.currentMobileActiveMenu = "feedgram-profile";
 
+  setTimeout(function () {
+    $scope.loadProfile();
+  },700);
+
+  $scope.loadProfile = function () {
+    startLoading();
+    var token = localStorageService.get("my_access_token");
+    var httpOptions = {
+      headers: {'Content-type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + token}
+    };
+    var params = {
+      "id": null
+    };
+    $http.post("http://127.0.0.1:9000/v1/feedgram/employee/getPageInfo", params, httpOptions)
+      .success(function (data, status, headers, config) {
+        $scope.info = data;
+        stopLoading();
+      }).catch(function (err) {
+      $rootScope.handleError(params, "/feedgram/employee/getPageInfo", err, httpOptions);
+    });
+  };
+
 });
 
