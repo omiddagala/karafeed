5db595f093fa6ca008fe05387be0ada261b12362
root <omid.javaheri66@gmail.com>
2019-04-14 17:31:43 +0430

    feedgram profile

--- www/js/feedgramProfileCtrl.js
+++ www/js/feedgramProfileCtrl.js
@@ -5,9 +5,15 @@ try {
   mymodule = angular.module("starter.controllers", []);
 }
 
-mymodule.controller('feedgramProfileCtrl', function ($scope, $compile, $http, localStorageService, $parse, $rootScope, $state) {
+mymodule.controller('feedgramProfileCtrl', function ($scope, $compile, $http, localStorageService, $parse, $rootScope, $ionicModal) {
   $rootScope.pageTitle = 'پروفایل';
   $rootScope.currentMobileActiveMenu = "feedgram-profile";
+  $ionicModal.fromTemplateUrl('app/pages/feedgram/profile/edit.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+  }).then(function (modal) {
+    $scope.editModal = modal;
+  });
 
   setTimeout(function () {
     $scope.loadProfile();
@@ -30,6 +36,25 @@ mymodule.controller('feedgramProfileCtrl', function ($scope, $compile, $http, lo
       $rootScope.handleError(params, "/feedgram/employee/getPageInfo", err, httpOptions);
     });
   };
+
+  $scope.doEdit = function () {
+    startLoading();
+    var token = localStorageService.get("my_access_token");
+    var httpOptions = {
+      headers: {'Content-type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + token}
+    };
+    var params = {
+      "value": $("#desc").val()
+    };
+    $http.post("http://127.0.0.1:9000/v1/feedgram/employee/editPageInfo", params, httpOptions)
+      .success(function (data, status, headers, config) {
+        $scope.editModal.hide();
+        $scope.info.description = params.value;
+        stopLoading();
+      }).catch(function (err) {
+      $rootScope.handleError(params, "/feedgram/employee/editPageInfo", err, httpOptions);
+    });
+  }
 
 });
 
